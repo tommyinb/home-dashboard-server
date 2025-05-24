ca817d1aa743fa4309c889b54b6711fa4ac0ef52
Tommy <tommy@tom.my>
2025-05-24 09:40:33 +0800

    remove ganso

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,8 +3,5 @@ initializeApp();
 
 export { hello } from "./hello";
 
-export { gansoWatch } from "./watches/gansoWatch";
-export { gansoSchedule } from "./watches/gansoSchedule";
-
 export { treasuryWatch } from "./treasuries/treasuryWatch";
 export { treasurySchedule } from "./treasuries/treasurySchedule";
--- functions/src/watches/gansoSchedule.ts
+++
@@ -1,6 +0,0 @@
-import { onSchedule } from "firebase-functions/scheduler";
-import { gansoCheck } from "./gansoCheck";
-
-export const gansoSchedule = onSchedule("*/20 * * * *", async (_event) => {
-  await gansoCheck();
-});
